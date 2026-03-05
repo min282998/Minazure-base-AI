fd6beccd47714ee1849f9076ca943b97ad9991f7
min282998 <haithanh231996@gmail.com>
2026-03-05 17:15:01 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,67 +1,17 @@
 plugins {
-    alias(libs.plugins.fabric.loom)
-}
-
-base {
-    archivesName = properties["archives_base_name"] as String
-    version = libs.versions.mod.version.get()
-    group = properties["maven_group"] as String
+    id("fabric-loom") version "1.7-SNAPSHOT"
 }
 
 repositories {
-    maven {
-        name = "meteor-maven"
-        url = uri("https://maven.meteordev.org/releases")
-    }
-    maven {
-        name = "meteor-maven-snapshots"
-        url = uri("https://maven.meteordev.org/snapshots")
-    }
+    mavenCentral()
+    maven("https://jitpack.io") // Link dự phòng khi server Meteor sập
+    maven("https://maven.fabricmc.net/")
 }
 
 dependencies {
-    // Fabric
-    minecraft(libs.minecraft)
-    mappings(variantOf(libs.yarn) { classifier("v2") })
-    modImplementation(libs.fabric.loader)
-
-    // Meteor
-    modImplementation(libs.meteor.client)
-}
-
-tasks {
-    processResources {
-        val propertyMap = mapOf(
-            "version" to project.version,
-            "mc_version" to libs.versions.minecraft.get()
-        )
-
-        inputs.properties(propertyMap)
-
-        filteringCharset = "UTF-8"
-
-        filesMatching("fabric.mod.json") {
-            expand(propertyMap)
-        }
-    }
-
-    jar {
-        inputs.property("archivesName", project.base.archivesName.get())
-
-        from("LICENSE") {
-            rename { "${it}_${inputs.properties["archivesName"]}" }
-        }
-    }
-
-    java {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
-
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        options.release = 21
-        options.compilerArgs.add("-Xlint:deprecation")
-        options.compilerArgs.add("-Xlint:unchecked")
-    }
+    minecraft("com.mojang:minecraft:1.21.4")
+    mappings("net.fabricmc:yarn:1.21.4+build.2:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.16.9")
+    // Tải Meteor trực tiếp từ GitHub cho chắc chắn
+    modImplementation("com.github.MeteorDevelopment:meteor-client:0.6.0-SNAPSHOT")
 }
