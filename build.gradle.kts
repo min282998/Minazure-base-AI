28e98db1e028c5bd318e5e6bd21e507869b232e7
min282998 <haithanh231996@gmail.com>
2026-03-05 17:26:38 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,21 @@ plugins {
     id("fabric-loom") version "1.7-SNAPSHOT"
 }
 
-version = "1.0.0"
-group = "com.example"
-
 repositories {
     mavenCentral()
-    maven("https://jitpack.io")
+    maven("https://maven.meteorclient.com/snapshots")
+    maven("https://maven.meteorclient.com/releases")
     maven("https://maven.fabricmc.net/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
     minecraft("com.mojang:minecraft:1.21.4")
     mappings("net.fabricmc:yarn:1.21.4+build.2:v2")
     modImplementation("net.fabricmc:fabric-loader:0.16.9")
-    // Dùng JitPack để né server Meteor đang sập
-    modImplementation("com.github.MeteorDevelopment:meteor-client:0.6.0-SNAPSHOT")
+    
+    // Quay lại dùng link chuẩn của Meteor, vì link com.github kia bị lỗi đường dẫn
+    modImplementation("meteordevelopment:meteor-client:0.6.0-SNAPSHOT")
 }
 
 tasks.withType<JavaCompile>().configureEach {
